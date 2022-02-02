38296ffd560a3b1a1373fcfd0f043aa1237a09c2
Wilhelm Wiberg <w@w5.se>
2022-02-02 10:34:39 +0100

    Say
    Just shorter than print. Also, time is under development.

--- ultimate-python.py
+++ ultimate-python.py
@@ -7,3 +7,12 @@ def hello_world():
 
 def exit():
     sys.exit()
+
+"""def time(time):
+    if time == "now" or time == 0:
+        return datetime.datetime.now()
+    else:
+        assert("[Errno 1]: That is actually a bad time.")"""
+
+def say(it):
+    print(it)
