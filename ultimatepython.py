fb0600f81abaca7725932314a883fe70225621db
Wilhelm Wiberg <w@w5.se>
2022-02-03 10:44:28 +0100

    Window
    You can now open windows.

---
+++ ultimatepython.py
@@ -0,0 +1,32 @@
+import sys
+import time
+import datetime
+import tkinter
+
+class WError(Exception):
+    def __init__(self, errno, msg):
+        super().__init__(f"[Errno {errno}]: {msg}")
+
+def hello_world():
+    print("Hello world!")
+
+def exit():
+    sys.exit()
+
+def time(time):
+    if time == "now" or time == 0:
+        return datetime.datetime.now()
+    else:
+        raise WError(1, "[That is actually a bad time.")
+def pause(time):
+    time.sleep(time)
+
+def say(it, time=0):
+    print(it)
+    pause(time)
+
+def open_window(title="My window", width1=100, height1=100, ):
+    window = tkinter.Tk(title)
+    window.configure(width=width1, height=height1)
+    window.configure(bg=background)
+    window.mainloop()
